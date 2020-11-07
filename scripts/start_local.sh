517ea2ff65480db3d98ba8bffc3a744020545881
Hideki Takeoka <iichikolamp@gmail.com>
2020-11-07 11:41:16 +0100

    Correct small issues

--- scripts/start_local.sh
+++ scripts/start_local.sh
@@ -8,4 +8,4 @@ _ip_addr=$(ip -o route get 8.8.8.8 | sed -e 's/^.* src \([^ ]*\) .*$/\1/')
 : ${ip_addr:=${_ip_addr}}
 : ${_port:=38459}
 
-hugo server -D -b "https://${ip_addr}" --bind ${ip_addr} --port ${_port} --disableFastRender
+hugo server -D -b "http://${ip_addr}" --bind ${ip_addr} --port ${_port} --disableFastRender
